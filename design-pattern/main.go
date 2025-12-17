cde4b2e98a5e86030f726745c189aa22ca1cbd42
Rayhan-abdullha <captainrayhan215@gmail.com>
2025-12-17 14:07:38 +0600

    singleton design pattern

--- design-pattern/main.go
+++ design-pattern/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"pattern/singleton"
 )
 
 func main() {
-	user := singleton.User{
-		Name:  "Rayhan",
-		Email: "rayhan@gmail",
-	}
-	singleton.GetInstance(user)
-	fmt.Println(user.GetUser())
+	singleton.SingleTon()
 }
--- design-pattern/singleton/singleton_design_pattern.go
+++ design-pattern/singleton/singleton_design_pattern.go
@@ -1,18 +1,36 @@
 package singleton
 
+import (
+	"fmt"
+	"sync"
+)
+
 type User struct {
 	Name  string
 	Email string
 }
 
-var instance *User
+var (
+	instance *User
+	once     sync.Once
+)
 
-func GetInstance(user User) *User {
-	if instance == nil {
-		instance = &user
-	}
+func GetInstance() *User {
+	once.Do(func() {
+		instance = &User{
+			Name:  "Rayhan",
+			Email: "rayhan@gmail",
+		}
+	})
 	return instance
 }
-func (u User) GetUser() User {
-	return u
+
+func SingleTon() {
+	u1 := GetInstance()
+	u2 := GetInstance()
+
+	u2.Name = "Abir"
+
+	fmt.Println(u1)
+	fmt.Println(u2)
 }
