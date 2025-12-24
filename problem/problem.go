2051e2494748cae575a369c027c29e8f0fd28df6
Rayhan-abdullha <captainrayhan215@gmail.com>
2025-12-24 11:00:06 +0600

    remove sum func

--- problem/problem.go
+++ problem/problem.go
@@ -1,23 +1,12 @@
 package problem
 
-import "fmt"
-
-func sum(arr []int) {
-	for i := 0; i < len(arr); i++ {
-		arr[i] += 10
-	}
-}
-
 func Problem() {
 	// SumArray([]int{1, 2, 3, 4, 5})
 
 	// FindMax([]int{1, 2, 33, 21, 34, 22})
 
-	arr := []int{1, 2, 3, 4, 5}
+	// arr := [5]int{1, 2, 3, 4, 5}
 	// fmt.Println("before", arr)
 	// Reverse_arr(arr)
 	// fmt.Println("after: ", arr)
-
-	sum(arr)
-	fmt.Println(arr)
 }
