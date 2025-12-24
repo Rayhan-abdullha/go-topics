b96d03dc27983d7af25f06c3c15f5f124c9dae15
Rayhan-abdullha <captainrayhan215@gmail.com>
2025-12-24 12:48:48 +0600

    fix

--- problem/problem.go
+++ problem/problem.go
@@ -1,12 +1,14 @@
 package problem
 
+import "fmt"
+
 func Problem() {
-	// SumArray([]int{1, 2, 3, 4, 5})
+	SumArray([]int{1, 2, 3, 4, 5})
 
-	// FindMax([]int{1, 2, 33, 21, 34, 22})
+	FindMax([]int{1, 2, 33, 21, 34, 22})
 
-	// arr := [5]int{1, 2, 3, 4, 5}
-	// fmt.Println("before", arr)
-	// Reverse_arr(arr)
-	// fmt.Println("after: ", arr)
+	arr := []int{1, 2, 3, 4, 5}
+	fmt.Println("before", arr)
+	Reverse_arr(arr)
+	fmt.Println("after: ", arr)
 }
--- problem/sum_array.go
+++ problem/sum_array.go
@@ -10,3 +10,9 @@ func SumArray(arr []int) int {
 	fmt.Println(sum)
 	return sum
 }
+func MillionLoop(c int) {
+	sum := 0
+	for i := 0; i < c; i++ {
+		sum += i
+	}
+}
